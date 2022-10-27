23b3149a4423bd94477181b940f2eac3ca37666a
VitorAssuncaoSantos <vitor.assuncaosantos100@gmail.com>
2022-10-27 18:19:09 -0300

    Mudanças no html

--- interhosp/js/main.js
+++ interhosp/js/main.js
@@ -1,30 +1,8 @@
-class MobileNavBar {
-    constructor(mobileMenu, navList) {
-        this.mobileMenu = document.querySelector(monileMenu)
-        this.navList = document.querySelector(navList)
-        this.navLinks = document.querySelectorAll(this.navLinks)
-        this.activeClass = "active"
-        this.handleClick = this.handleClick.bind()
+const nome = document.getElementById(nome)
+const xhttp = new XMLHttpRequest;
 
-    }
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick())
-    }
+xhttp.onreadystatechange = ()=>{
 
-    handleClick(){
-        console.log(this)
-        this.navList.classList.touggle(this.activeClass)
-    }
-
-    init(this.mobileMenu) {
-        this.addClickEvent()
-    }
-    return this;
 }
 
-const mobileNavbar = new MobileNavBar(
-    "#mobile-menu",
-    "#nav-list",
-    "#nav-list li",
-)
-mobileNavbar.init()
+xhttp.open("GET","../php/conexo")
